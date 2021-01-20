39cdee74043b407756c5986ac97f0b35a865345b
Felipe Guimarães <39745145+Felipep31a@users.noreply.github.com>
2021-01-20 13:16:56 -0300

    Create next.config.js
    env

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env:{
+        "BASE_URL":"http://localhost:3000",
+        "MONGODB_URL": "",
+        "ACCESS_TOKEN_SECRET": "",
+        "REFRESH_TOKEN_SECRET": "",
+    },
+}
